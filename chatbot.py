a145b0a2520ec285b4c7230b8d141a9319da02fb
Paridhisharmagithub <sparidhi161.ps@gmail.com>
2025-09-21 23:19:10 +0530

    changed front end link

--- chatbot.py
+++ chatbot.py
@@ -24,7 +24,7 @@ app = FastAPI(title="AI ChatBot API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["https://document-parser-snowy.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
